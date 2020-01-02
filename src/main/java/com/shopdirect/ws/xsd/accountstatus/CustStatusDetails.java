0a3e8b4457f2b028ac429f225496e8953fcd5fa2
ardlal1989 <50587555+ardlal1989@users.noreply.github.com>
2020-01-02 12:37:39 +0530

    Update CustStatusDetails.java

--- src/main/java/com/shopdirect/ws/xsd/accountstatus/CustStatusDetails.java
+++ src/main/java/com/shopdirect/ws/xsd/accountstatus/CustStatusDetails.java
@@ -73,7 +73,7 @@ public class CustStatusDetails {
         return this.comLsdgWsAccountStatusAccountStatusObjectsCustStatusDetails;
     }
 
-	public void setComLsdgWsAccountStatusAccountStatusObjectsCustStatusDetails(
+	public void setCustStatusDetails(
 			List<ComLsdgWsAccountStatusAccountStatusObjectsCustStatusDetails> comLsdgWsAccountStatusAccountStatusObjectsCustStatusDetails) {
 		this.comLsdgWsAccountStatusAccountStatusObjectsCustStatusDetails = comLsdgWsAccountStatusAccountStatusObjectsCustStatusDetails;
 	}
